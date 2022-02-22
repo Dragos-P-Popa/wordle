d5ee9e7104673aa3cc510ee5d82b662a31d9ac42
Dragos Popa <ed20dpp@leeds.ac.uk>
2022-02-22 21:32:22 +0000

    Initial implementation of Game.class. Untested ATM.

--- coursework/cwk1/src/main/java/comp1721/cwk1/Game.java
+++ coursework/cwk1/src/main/java/comp1721/cwk1/Game.java
@@ -1,16 +1,33 @@
 package comp1721.cwk1;
 
 
+import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Game {
+  public String word;
+  public WordList list;
+  public LocalDateTime initialDate, currentDate;
+  public DateTimeFormatter formatForDate;
 
   // TODO: Implement constructor with String parameter
-  public Game(String location){
+  public Game(String location) throws IOException {
+    formatForDate = DateTimeFormatter.ofPattern("dd MM yyyy");
+    initialDate = LocalDateTime.from(LocalDate.parse("19 06 2021", formatForDate));
+    currentDate = LocalDateTime.from(LocalDate.parse("10 02 2022", formatForDate));
+    long daysBetween = Duration.between(initialDate, currentDate).toDays();
 
+    list = new WordList(location);
+    word = list.getWord((int) daysBetween);
   }
 
   // TODO: Implement constructor with int and String parameters
-  public Game(int i, String s){
-
+  public Game(int i, String location) throws IOException {
+    list = new WordList(location);
+    word = list.getWord(i);
   }
 
   // TODO: Implement play() method
