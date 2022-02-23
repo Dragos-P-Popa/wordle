32950e376d483bec17d996fb3e4c115847dd18ca
Dragos Popa <ed20dpp@leeds.ac.uk>
2022-02-23 20:04:10 +0000

    Completed matches();, compareWith(); getChosenWord(); Guess(int, string);

--- coursework/cwk1/src/main/java/comp1721/cwk1/Game.java
+++ coursework/cwk1/src/main/java/comp1721/cwk1/Game.java
@@ -1,24 +1,29 @@
 package comp1721.cwk1;
 
 
+import javax.swing.text.DateFormatter;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.Month;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class Game {
   public String word;
   public WordList list;
-  public LocalDateTime initialDate, currentDate;
-  public DateTimeFormatter formatForDate;
+  public LocalDate initialDate;
+  public LocalDate currentDate;
+  public DateFormatter formatForDate;
 
   // TODO: Implement constructor with String parameter
   public Game(String location) throws IOException {
-    formatForDate = DateTimeFormatter.ofPattern("dd MM yyyy");
-    initialDate = LocalDateTime.from(LocalDate.parse("19 06 2021", formatForDate));
-    currentDate = LocalDateTime.from(LocalDate.parse("10 02 2022", formatForDate));
-    long daysBetween = Duration.between(initialDate, currentDate).toDays();
+
+    DateTimeFormatter df = DateTimeFormatter.ofPattern("dd MM yyyy");
+    LocalDate dateBefore = LocalDate.parse("19 06 2021", df);
+    LocalDate dateAfter = LocalDate.parse("10 02 2022", df);
+    long daysBetween = ChronoUnit.DAYS.between(dateBefore, dateAfter);
 
     list = new WordList(location);
     word = list.getWord((int) daysBetween);
--- coursework/cwk1/src/main/java/comp1721/cwk1/Guess.java
+++ coursework/cwk1/src/main/java/comp1721/cwk1/Guess.java
@@ -1,37 +1,44 @@
 package comp1721.cwk1;
 
 import java.util.Arrays;
+import java.util.Locale;
+import java.util.Objects;
 import java.util.Scanner;
 
 
 public class Guess {
   // Use this to get player input in readFromPlayer()
   private static final Scanner INPUT = new Scanner(System.in);
+  public String chosenWord;
+  public int guessNumber = 0;
 
   // TODO: Implement constructor with int parameter
   public Guess(int GuessNumber){
     if (GuessNumber < 1 || GuessNumber > 6){
-      throw new GameException("ERROR");
+      throw new GameException("");
     }
+    guessNumber = guessNumber + 1;
   }
 
   // TODO: Implement constructor with int and String parameters
-  public Guess(int GuessNumber, String chosenWord){
-    /**if ((GuessNumber < 1 || GuessNumber > 6) || (!chosenWord.toUpperCase().matches("[A-Z]")) || (chosenWord.length() != 5)) {
-      throw new GameException("ERROR");
-    }**/
+  public Guess(int GuessNumber, String Cword){
+    if ((GuessNumber < 1 || GuessNumber > 6) || (!Cword.matches("[a-zA-Z]+")) || (Cword.length() != 5)) {
+      throw new GameException("");
+    }
+    guessNumber = guessNumber + 1;
+    chosenWord = Cword.toUpperCase();
   }
 
   // TODO: Implement getGuessNumber(), returning an int
   public int getGuessNumber(){
 
-  return 0;
+  return guessNumber;
   }
 
   // TODO: Implement getChosenWord(), returning a String
   public String getChosenWord(){
 
-  return "";
+  return chosenWord;
   }
 
   // TODO: Implement readFromPlayer()
@@ -47,13 +54,13 @@ public class Guess {
 
     for (int i = 0; i < 5; i++) {
       if(Arrays.asList(word).contains(guess[i]) && Arrays.asList(word).indexOf(guess[i]) == i){
-        output = output + "\033[30;102m "+ guess[i] +" \033[0m";
+        output = output + "\033[30;102m "+ word[i] +" \033[0m";
       }
       if(Arrays.asList(word).contains(guess[i]) && Arrays.asList(word).indexOf(guess[i]) != i){
-        output = output + "\033[30;103m "+ guess[i] +" \033[0m";
+        output = output + "\033[30;103m "+ word[i] +" \033[0m";
       }
       if(!Arrays.asList(word).contains(guess[i])){
-        output = output + "\033[30;107m "+ guess[i] +" \033[0m";
+        output = output + "\033[30;107m "+ word[i] +" \033[0m";
       }
     }
   return output;
@@ -61,7 +68,10 @@ public class Guess {
 
   // TODO: Implement matches(), giving it a String parameter and boolean return type
   public Boolean matches(String s){
-
-  return true;
+    if (Objects.equals(s, getChosenWord())){
+      return true;
+    }else{
+      return false;
+    }
   }
 }
