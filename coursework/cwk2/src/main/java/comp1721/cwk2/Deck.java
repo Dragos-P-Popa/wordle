6a909b59f486f800759bb5184644d101ff4ca31c
Dragos Popa <ed20dpp@leeds.ac.uk>
2022-05-05 11:53:16 +0100

    Basic solution - stubs

--- coursework/cwk2/src/main/java/comp1721/cwk2/Deck.java
+++ coursework/cwk2/src/main/java/comp1721/cwk2/Deck.java
@@ -1,3 +1,37 @@
 package comp1721.cwk2;
 
-// Implement Deck class here
+public class Deck {
+
+    public Deck(){
+
+    }
+
+    public int size(){
+
+        return 0;
+    }
+
+    public boolean isEmpty(){
+
+        return false;
+    }
+
+    public boolean contains(String card){
+
+        return false;
+    }
+
+    public void discard(){
+
+    }
+
+    public String deal(){
+        //remove card from deck and return it
+        return "";
+    }
+
+    public void shuffle(){
+
+    }
+}
+
--- coursework/cwk2/src/main/java/comp1721/cwk2/PokerHand.java
+++ coursework/cwk2/src/main/java/comp1721/cwk2/PokerHand.java
@@ -1,3 +1,57 @@
 package comp1721.cwk2;
 
-// Implement PokerHand class here
+public class PokerHand {
+
+    public PokerHand(){
+
+    }
+
+    public PokerHand(String hand){
+
+    }
+
+    public String toString(){
+        return "";
+    }
+
+    public int size(){
+
+        return 0;
+    }
+
+    public void discard(){
+
+    }
+
+    public void discardTo(Deck deck){
+
+    }
+
+    public boolean isPair(){
+        return false;
+    }
+
+    public boolean isFullHouse(){
+        return false;
+    }
+
+    public boolean isTwoPairs(){
+        return false;
+    }
+
+    public boolean isFlush(){
+        return false;
+    }
+
+    public boolean isThreeOfAKind(){
+        return false;
+    }
+
+    public boolean isStraight(){
+        return false;
+    }
+
+    public boolean isFourOfAKind(){
+        return false;
+    }
+}
