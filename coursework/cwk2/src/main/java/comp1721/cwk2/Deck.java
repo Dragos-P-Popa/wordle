6fb5d2eb293a0c801c36412b14b291e226373133
Dragos Popa <ed20dpp@leeds.ac.uk>
2022-05-20 21:59:23 +0100

    Changed some comments so they show in the javadoc

--- coursework/cwk2/src/main/java/comp1721/cwk2/Deck.java
+++ coursework/cwk2/src/main/java/comp1721/cwk2/Deck.java
@@ -1,13 +1,20 @@
 package comp1721.cwk2;
 
-import java.lang.reflect.Array;
 import java.util.*;
+
+/**
+ * Representation of a deck of playing cards.
+ *
+ * @author Dragos Popa
+ */
 
 public class Deck {
     public String[] deckString;
 
+    /**
+     * Initialising string array deckString which will hold all the cards currently in the deck.
+     */
     public Deck(){
-        //Initialising string array deckString which will hold all of the cards currently in the deck.
         deckString = new String[]{"AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "TC", "JC", "QC", "KC",
                 "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "TH", "JH", "QH", "KH",
                 "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "TD", "JD", "QD", "KD",
@@ -16,16 +23,20 @@ public class Deck {
 
     }
 
+    /**
+     * Returns size of the deck
+     * Calculated by filtering out all null items
+     * then counting what is left over
+     * when removing a card from a deck it is replaced by Null
+     */
     public int size(){
-        //return size of the deck
-        //calculated by filtering out all null items
-        //then counting what is left over
-        //when removing a card from a deck it is replaced by Null
         return (int) Arrays.stream(deckString).filter(Objects::nonNull).count();
     }
 
+    /**
+     * Simple checker to see whether the array is empty, excluding null items
+     */
     public boolean isEmpty(){
-        //simple check wether the array is empty, excluding null items
         if (this.size() > 0) {
             return false;
         } else {
@@ -33,11 +44,18 @@ public class Deck {
         }
     }
 
+    /**
+     * Checks if a certain card is present in the deck
+     * @param card
+     */
     public boolean contains(Card card){
-        //checks if a certain card is present in the deck
         return Arrays.asList(deckString).contains(card.toString());
     }
 
+    /**
+     * Adds a new card to the deck
+     * @param card
+     */
     public void add(Card card){
         //add new card to the deck
         //iterate 52 times, max possible
@@ -51,11 +69,16 @@ public class Deck {
         }
     }
 
+    /**
+     * Replaces all items in the array will Null value
+     */
     public void discard(){
-        //replaces all items in the array will Null value
         Arrays.fill(deckString, null);
     }
 
+    /**
+     * Deals a card from the deck
+     */
     public Card deal(){
         //iterates throughout the whole array
         for (int i = 0; i < 52; i++) {
@@ -72,6 +95,9 @@ public class Deck {
         throw new CardException("The deck is empty");
     }
 
+    /**
+     * Shuffles the deck
+     */
     public void shuffle(){
         Random rand = new Random();
 
--- coursework/cwk2/src/main/java/comp1721/cwk2/PokerHand.java
+++ coursework/cwk2/src/main/java/comp1721/cwk2/PokerHand.java
@@ -2,15 +2,29 @@ package comp1721.cwk2;
 
 import java.util.*;
 
+
+/**
+ * Representation of a poker hand.
+ *
+ * @author Dragos Popa
+ */
+
 public class PokerHand {
     public static int FULL_SIZE = 52;
     public String[] handString;
 
 
+    /**
+     * Creates an empty poker hand object
+     */
     public PokerHand(){
         handString = new String[]{null, null, null, null, null};
     }
 
+    /**
+     * Creates a poker hand using the given string to populate it
+     * @param hand
+     */
     public PokerHand(String hand){
         String[] splitHand = hand.split("\\s+");
         if (splitHand.length > 5){
@@ -21,6 +35,10 @@ public class PokerHand {
         System.arraycopy(splitHand, 0, handString, 0, splitHand.length);
     }
 
+    /**
+     * Adds a card to the poker hand
+     * @param card
+     */
     public void add(Card card){
         if (this.size() == 5){
             throw new CardException("Hand is full");
@@ -41,6 +59,10 @@ public class PokerHand {
         }
     }
 
+    /**
+     * Returns the poker hand in string format
+     * @return
+     */
     public String toString(){
         String retString = "";
         for (int i = 0; i < handString.length; i++) {
@@ -57,10 +79,17 @@ public class PokerHand {
         return retString;
     }
 
+    /**
+     * Returns the amount of card held in the hand currently
+     * @return
+     */
     public int size(){
         return (int) Arrays.stream(handString).filter(Objects::nonNull).count();
     }
 
+    /**
+     * Discards all cards from the hand
+     */
     public void discard(){
         if ((int) Arrays.stream(handString).filter(Objects::nonNull).count() != 0){
             Arrays.fill(handString, null);
@@ -69,6 +98,10 @@ public class PokerHand {
         }
     }
 
+    /**
+     * Discards the card from the hand and adds it back into the deck
+     * @param deck
+     */
     public void discardTo(Deck deck){
         if ((int) Arrays.stream(handString).filter(Objects::nonNull).count() != 0){
             for (int i = 0; i < 5; i++) {
@@ -81,6 +114,7 @@ public class PokerHand {
             throw new CardException("Hand is empty");
         }
     }
+
 
     public boolean isPair(){
         List handList = Arrays.asList(handString);
