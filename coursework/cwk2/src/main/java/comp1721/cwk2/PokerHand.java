39b194eb0f6b004e2ab98f430c8adb6ad58b0159
Dragos Popa <ed20dpp@leeds.ac.uk>
2022-05-20 12:04:09 +0100

    PokerHand.java improvements. Errors for full hand, discard(), correct representation in string format, hand creation validation, add()

--- coursework/cwk2/src/main/java/comp1721/cwk2/PokerHand.java
+++ coursework/cwk2/src/main/java/comp1721/cwk2/PokerHand.java
@@ -1,33 +1,67 @@
 package comp1721.cwk2;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
 public class PokerHand {
     public static int FULL_SIZE = 52;
     public String[] handString;
 
 
     public PokerHand(){
-        handString = new String[5];
+        handString = new String[]{null, null, null, null, null};
     }
 
     public PokerHand(String hand){
-
+        String[] splitHand = hand.split("\\s+");
+        if (splitHand.length > 5){
+            throw new CardException("Too many cards.");
+        }
+        handString = new String[]{null, null, null, null, null};
+        System.arraycopy(splitHand, 0, handString, 0, splitHand.length);
     }
 
     public void add(Card card){
-
+        if (this.size() == 5){
+            throw new CardException("Hand is full");
+        }
+        for (int i = 0; i < 5; i++) {
+            if (handString[i] ==  "" + card.getRank() + card.getSuit()){
+                throw new CardException("Duplicate card");
+            }
+        }
+        for (int i = 0; i < 5; i++) {
+            if (handString[i] == null){
+                handString[i] = "" + card.getRank() + card.getSuit();
+                break;
+            }
+        }
     }
 
     public String toString(){
-        return "";
+        String retString = "";
+        for (int i = 0; i < handString.length; i++) {
+            if (handString[i] == null){
+                continue;
+            }
+            retString = retString + handString[i] + " ";
+        }
+        if (retString.length() >= 3){
+            StringBuilder withoutSpace = new StringBuilder(retString);
+            withoutSpace.deleteCharAt(retString.length()-1);
+            return String.valueOf(withoutSpace);
+        }
+        return retString;
     }
 
     public int size(){
-
-        return 0;
+        return (int) Arrays.stream(handString).filter(Objects::nonNull).count();
     }
 
     public void discard(){
-
+        Arrays.fill(handString, null);
     }
 
     public void discardTo(Deck deck){
